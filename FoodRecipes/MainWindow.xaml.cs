0d487979e710058c2aec442d80543b352a97ad56
TranHuynh1999 <57291457+TranHuynh1999@users.noreply.github.com>
2020-10-23 16:12:28 +0700

    Build

---
+++ FoodRecipes/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FoodRecipes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+            public MainWindow()
+            {
+                InitializeComponent();
+            }
+            class Food
+            {
+                public string Name { get; set; }
+                public string Avatar { get; set; }
+
+            }
+            class FoodDAO
+            {
+                public static BindingList<Food> GetAllFood()
+                {
+                    var result = new BindingList<Food>()
+                {
+                    new Food() { Name = "Bach Tuoc Ngam sa", Avatar = "/Images/Bachtuocngamsa.jpg"},
+                    new Food() { Name = "Ca nuc kho nuoc dua", Avatar = "/Images/Canuckhonuocdua.jpg" },
+                    new Food() { Name = "Chan gio ham", Avatar = "/Images/Changioham.jpg" },
+                    new Food() { Name = "Dau hu sot nam huong", Avatar = "/Images/Dauhusotnamhuong.jpg" },
+                    new Food() { Name = "Ga chien han quoc", Avatar = "/Images/Gachienhanquoc.jpg" },
+                    new Food() { Name = "Muc xao sot cay", Avatar = "/Images/Mucxaosotcay.jpg" },
+                    new Food() { Name = "Nui sot tom", Avatar = "/Images/Nuisottom.jpg" },
+                    new Food() { Name = "Thit ga hap nam huong", Avatar = "/Images/Thitgahapnamhuong.jpg" },
+                    new Food() { Name = "Tom rang muoi tieu", Avatar = "/Images/Tomrangmuoitieu.jpg" }
+                };
+                    return result;
+                }
+            }
+            BindingList<Food> _listFood = new BindingList<Food>();
+
+            private void BindingFood(object sender, RoutedEventArgs e)
+            {
+                _listFood = FoodDAO.GetAllFood();
+                DataListview.ItemsSource = _listFood;
+            }
+
+        private void ButtonFechar_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ListViewMenu_Selectionchanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = ListViewMenu.SelectedIndex;
+            MoveCursorMenu(index);
+        }
+
+        private void MoveCursorMenu(int index)
+        {
+            TransitioningContenSlide.OnApplyTemplate();
+            GridCorsor.Margin = new Thickness(0,(100+(60*index)),0,0);
+        }
+
+        private void ButtonOpen_Click(object sender, RoutedEventArgs e)
+        {
+            MainData.Margin = new Thickness(250, 0, 0, 0);
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            MainData.Margin = new Thickness(0, 0, 0, 0);
+        }
+    }
+    }
+
---
+++ FoodRecipes/SplashWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FoodRecipes
+{
+    /// <summary>
+    /// Interaction logic for SplashWindow.xaml
+    /// </summary>
+    public partial class SplashWindow : Window
+    {
+        Random _rng = new Random();
+        public SplashWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void neverShowAgain_Click(object sender, RoutedEventArgs e)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings["ShowSplashScreen"].Value = "false";
+            config.Save(ConfigurationSaveMode.Minimal);
+        }
+
+        System.Timers.Timer timer;
+
+        private void window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            var value = ConfigurationManager.AppSettings["ShowSplashScreen"];
+            var showSplash = bool.Parse(value);
+            if (showSplash == false)
+            {
+                var screen = new MainWindow();
+                screen.Show();
+                this.Close();
+            }
+            else
+            {
+                timer = new System.Timers.Timer();
+                timer.Elapsed += Timer_Elapsed;
+                timer.Interval = 1000;
+                timer.Start();
+                readData();
+            }
+        }
+        int cout = 0;
+        int target = 5;
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            cout++;
+            if (cout == target)
+            {
+                timer.Stop();
+                Dispatcher.Invoke(() =>
+                {
+                    var screen = new MainWindow();
+                    screen.Show();
+                    this.Close();
+                });
+            }
+        }
+
+  
+        private void readData()
+        {
+            //Uri path = new Uri(@"Data.txt", UriKind.RelativeOrAbsolute);
+
+            //string[] lines = System.IO.File.ReadAllLines(path.ToString());
+            //int result = _rng.Next(lines.Length);
+
+            //interestingInformation.Text = lines[result];
+            try
+            {
+                string stringPath = @"Data\DataInformationInteresting.txt";
+
+                string[] lines = System.IO.File.ReadAllLines(stringPath);
+                int result = _rng.Next(lines.Length);
+                interestingInformation.Text = lines[result];
+            }
+            catch (Exception) { }
+        }
+
+        //public static List<string> docFile(string path)
+        //{
+        //    List<string> result = new List<string>();
+        //    try
+        //    {
+        //        using (StreamReader sr = new StreamReader(path))
+        //        {
+        //            string line;
+        //            while ((line = sr.ReadLine()) != null)
+        //            {
+        //                result.Add(line);
+        //            }
+        //        }
+
+        //    }
+        //    catch (Exception)
+        //    { }
+        //    return result;
+        //}
+
+
+        private void Continue_Click(object sender, RoutedEventArgs e)
+        {
+            var screen = new MainWindow();
+            screen.Show();
+            this.Close();
+            timer.Stop();
+        }
+    }
+}
