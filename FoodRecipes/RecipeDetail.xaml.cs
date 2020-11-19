924b59ebea9076a3497a308f5fd04888f4154581
kptankhoa <kptankhoa@gmail.com>
2020-11-19 15:02:43 +0700

    fix ui & and link handler

--- FoodRecipes/RecipeDetail.xaml.cs
+++ FoodRecipes/RecipeDetail.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace FoodRecipes
@@ -37,18 +38,32 @@ namespace FoodRecipes
             favoriteIcon.Foreground = (isFavorite) ? Brushes.Red : Brushes.Gray;
 
             RecipeNameTextBlock.Text = this.SelectedRecipe.Name + "\n";
+            if (this.SelectedRecipe.VideoLink != "") { 
+                YTlink.NavigateUri = new Uri(this.SelectedRecipe.VideoLink, UriKind.Absolute);
+            }
+            else
+            {
+                YTlink.Click += nullclickhandler;
+            }
 
-            foreach(String s in SelectedRecipe.Ingredients)
+            foreach (String s in SelectedRecipe.Ingredients)
             {
                 IngredientTextBlock.Text += s + "\n";
             }
-            StepDescriptionTextBlock.Text = SelectedRecipe.Step[currentStep].Description;
+            StepDescriptionTextBlock.Text = $"Bước {currentStep + 1}: " + SelectedRecipe.Step[currentStep].Description;
             try
             {
+                if (SelectedRecipe.Step[currentStep].Images != "") { 
                 RelativeToAbsoluteConverter converter = new RelativeToAbsoluteConverter();
                 String absolutePath = (String)converter.Convert(SelectedRecipe.Step[currentStep].Images, null, null, null);
                 var image = new BitmapImage(new Uri(absolutePath));
                 StepImage.Source = image;
+                }
+                else
+                {
+                    StepImage.Source = null;
+                }    
+
             }
             catch(Exception ex)
             {
@@ -57,19 +72,37 @@ namespace FoodRecipes
 
         }
 
+        private void nullclickhandler(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Chưa có video hướng dẫn", "Sorry!");
+            
+        }
+
         private void PreStep_Button_Click(object sender, RoutedEventArgs e)
         {
             if(currentStep > 0)
             {
                 currentStep --;
-                StepDescriptionTextBlock.Text = SelectedRecipe.Step[currentStep].Description;
+                StepDescriptionTextBlock.Text = $"Bước {currentStep + 1}: " + SelectedRecipe.Step[currentStep].Description;
+               // StepDescriptionTextBlock.Text = SelectedRecipe.Step[currentStep].Description;
 
                 try
                 {
-                    RelativeToAbsoluteConverter converter = new RelativeToAbsoluteConverter();
-                    String absolutePath = (String)converter.Convert(SelectedRecipe.Step[currentStep].Images, null, null, null);
-                    var image = new BitmapImage(new Uri(absolutePath));
-                    StepImage.Source = image;
+                    //RelativeToAbsoluteConverter converter = new RelativeToAbsoluteConverter();
+                    //String absolutePath = (String)converter.Convert(SelectedRecipe.Step[currentStep].Images, null, null, null);
+                    //var image = new BitmapImage(new Uri(absolutePath));
+                    //StepImage.Source = image;
+                    if (SelectedRecipe.Step[currentStep].Images != "")
+                    {
+                        RelativeToAbsoluteConverter converter = new RelativeToAbsoluteConverter();
+                        String absolutePath = (String)converter.Convert(SelectedRecipe.Step[currentStep].Images, null, null, null);
+                        var image = new BitmapImage(new Uri(absolutePath));
+                        StepImage.Source = image;
+                    }
+                    else
+                    {
+                        StepImage.Source = null;
+                    }
                 }
                 catch (Exception ex)
                 {
@@ -83,14 +116,27 @@ namespace FoodRecipes
             if (currentStep < SelectedRecipe.Step.Count - 1)
             {
                 currentStep ++;
-                StepDescriptionTextBlock.Text = SelectedRecipe.Step[currentStep].Description;
+                //StepDescriptionTextBlock.Text = SelectedRecipe.Step[currentStep].Description;
+                StepDescriptionTextBlock.Text = $"Bước {currentStep + 1}: " + SelectedRecipe.Step[currentStep].Description;
+
 
                 try
                 {
-                    RelativeToAbsoluteConverter converter = new RelativeToAbsoluteConverter();
-                    String absolutePath = (String)converter.Convert(SelectedRecipe.Step[currentStep].Images, null, null, null);
-                    var image = new BitmapImage(new Uri(absolutePath));
-                    StepImage.Source = image;
+                    //RelativeToAbsoluteConverter converter = new RelativeToAbsoluteConverter();
+                    //String absolutePath = (String)converter.Convert(SelectedRecipe.Step[currentStep].Images, null, null, null);
+                    //var image = new BitmapImage(new Uri(absolutePath));
+                    //StepImage.Source = image;
+                    if (SelectedRecipe.Step[currentStep].Images != "")
+                    {
+                        RelativeToAbsoluteConverter converter = new RelativeToAbsoluteConverter();
+                        String absolutePath = (String)converter.Convert(SelectedRecipe.Step[currentStep].Images, null, null, null);
+                        var image = new BitmapImage(new Uri(absolutePath));
+                        StepImage.Source = image;
+                    }
+                    else
+                    {
+                        StepImage.Source = null;
+                    }
                 }
                 catch (Exception ex)
                 {
@@ -117,6 +163,15 @@ namespace FoodRecipes
             }
         }
 
+        private void ButtonFechar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
 
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
     }
 }
